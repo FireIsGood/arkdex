e8ed5bcb0bdd058ad8d25dd6b567e1aacbb409c7
FireIsGood <fireisadequate@gmail.com>
2022-10-10 20:42:48 -0700

    Install packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
-
 import image from "@astrojs/image";
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,9 +11,7 @@ export default defineConfig({
   site: "https://fireisgood.github.io/",
   base: "/arkdex",
   trailingSlash: "ignore",
-  integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), svelte()]
 });
