8fd0346d011d6b567c224dba12d05456f33a7633
FireIsGood <fireisadequate@gmail.com>
2022-10-10 20:53:43 -0700

    Add swup things

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,3 +3,5 @@ declare module "swup";
 declare module "@swup/head-plugin";
 declare module "@swup/a11y-plugin";
 declare module "@swup/scroll-plugin";
+declare module "@swup/body-class-plugin";
+declare module "@swup/scripts-plugin";
