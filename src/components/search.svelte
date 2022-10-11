7897c68dd90651638e1f9c238ed4fa57d27b91a9
FireIsGood <fireisadequate@gmail.com>
2022-10-10 23:23:28 -0700

    Add search secrets

--- src/components/search.svelte
+++ src/components/search.svelte
@@ -2,6 +2,23 @@
   import { searchTerm } from "@components/search-store";
   import { removeSpace } from "@scripts/slugGen";
 
+  import amogus from "../assets/amogus.gif?url";
+  import lappdumb from "../assets/lappdumb.png?url";
+  import sheew from "../assets/sheew.webp?url";
+  import blitzsmile from "../assets/blitzsmile.png?url";
+  import bernard from "../assets/bernard.png?url";
+  import rick from "../assets/code.png?url";
+
+  const secrets = <any>{
+    amogus: amogus,
+    sussy: amogus,
+    lappdumb: lappdumb,
+    sheew: sheew,
+    blitzsmile: blitzsmile,
+    bernard: bernard,
+    rick: rick,
+  };
+
   let input: string;
 
   $: {
@@ -15,12 +32,32 @@
 
 <div class="container">
   <input type="text" placeholder="Search Operators" bind:value={input} />
+  <div class:secret={!($searchTerm in secrets)}>
+    <div class="image">
+      <img src={secrets[$searchTerm]} alt="oh no" />
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
   .container {
     display: flex;
-    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .secret {
+    display: none;
+  }
+  .image {
+    padding-top: 1rem;
+    width: max(8rem, 25%);
+    margin-inline: auto;
+
+    img {
+      width: 100%;
+      object-fit: cover;
+    }
   }
 
   input {
